c6a2dbf3d8e1e47039bcafafc426fcc02bcd51dc
Renato Balcazar <renatojbl99@gmail.com>
2018-10-31 21:38:03 -0500

    Subiendo ejercicios

---
+++ PA_Semana_05_Ttutoria/paquete/modelado.py
@@ -0,0 +1,31 @@
+class Persona(object):
+	def __init__(self):
+		self.nombres = ""
+		self.edad = 0
+
+	def agregar_nombres(self,n):
+		self.nombres = n
+
+	def agregar_edad(self,n):
+		self.edad = n
+
+	def obtener_nombres(self):
+		return self.nombres
+
+	def obtener_edad(self):
+		return self.edad
+
+	def presentar_datos(self):
+		c = "%s - %s" % (self.obtener_edad(), self.obtener_nombres())
+		return c
+
+
+class Estudiante(Persona):
+	def __init__(self):
+		super(Estudiante,self).__init__()
+		self.nota = 0
+
+	def presentar_datos(self):
+		c = "%s - %s" % (super(Estudiante,self).presentar_datos(), self.nota)
+		return c
+
